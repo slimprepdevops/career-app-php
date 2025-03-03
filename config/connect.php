5f4e5c5279b5924060f18584454aa5965acefdc2
Benjamin Isong <47520669+beisong7@users.noreply.github.com>
2025-03-03 10:03:16 +0800

    Update connect.php

--- config/connect.php
+++ config/connect.php
@@ -1,5 +1,8 @@
 <?php
 
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+
     $host = "localhost";
     $username = "root";
     $password = "";
@@ -12,4 +15,3 @@
     if (!$conn) {
         die("Connection failed: " . mysqli_connect_error());
     }
-
